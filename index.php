8d8ee502ac892793405b3dbe2c9dc4074da72533
Maria Cavalcante <101808675+MariaEduCavalcante@users.noreply.github.com>
2022-09-03 21:53:40 -0300

    carregando informações do banco de dados

--- index.php
+++ index.php
@@ -24,40 +24,45 @@
 
 <body>
  
-    <?php include 'nav.php'?>
-    <?php include 'cabecalho.html'?>
+    <?php include 'nav.php';
+          include 'cabecalho.html';
+          include 'conexao.php';
+          
+          $consulta = $cn->query('select nm_livro,vl_preco,ds_capa,qt_estoque from vw_livro');
+          
+          ?>
 
     <div class="container-fluid">
         <div class="row">
+            <?php 
+            while($exibe = $consulta->fetch(PDO::FETCH_ASSOC)){
+            ?>
             <div class="col-sm-3">
-                <img src="https://placehold.co/450x320" class="img-responsive" style="width:100%">
-                <div><h1>Nome do Produto</h1></div>
-                <div><h4>R$500,00</h4></div>
-            </div>
+                <img src="img/<?php echo $exibe['ds_capa']; ?>.jpg" class="img-responsive" style="width:100%">
+                <div><h4><b><?php echo mb_strimwidth ($exibe['nm_livro'],0,30,'...'); ?></b></h4></div>
+                <div><h5>R$ <?php echo number_format ($exibe['vl_preco'],2,',','.'); ?></h5></div>
+            
+                <div class="text-center">
+                    <button class="btn btn-lg btn-block btn-info">
+                        <span class="glyphicon glyphicon-info-sign" > Detalhes</span>
+                    </button>
+                </div>
 
-            <div class="col-sm-3">
-                <img src="https://placehold.co/450x320" class="img-responsive" style="width:100%">
-                <div><h1>Nome do Produto</h1></div>
-                <div><h4>R$500,00</h4></div>
+                <div class="text-center" style="margin-top:5px; margin-bottom:5px;">
+                    <?php
+                    if($exibe['qt_estoque'] > 0){
+                    ?>
+                    <button class="btn btn-lg btn-block btn-primary">
+                        <span class="glyphicon glyphicon-usd"> Comprar</span>
+                    </button>
+                    <?php } else { ?>
+                    <button class="btn btn-lg btn-block btn-primary" disabled>
+                        <span class="glyphicon glyphicon-remove-circle"> Indisponível</span>
+                    </button>
+                    <?php } ?>
+                </div>
             </div>
-
-            <div class="col-sm-3">
-                <img src="https://placehold.co/450x320" class="img-responsive" style="width:100%">
-                <div><h1>Nome do Produto</h1></div>
-                <div><h4>R$500,00</h4></div>
-            </div>
-
-            <div class="col-sm-3">
-                <img src="https://placehold.co/450x320" class="img-responsive" style="width:100%">
-                <div><h1>Nome do Produto</h1></div>
-                <div><h4>R$500,00</h4></div>
-            </div>
-
-            <div class="col-sm-3">
-                <img src="https://placehold.co/450x320" class="img-responsive" style="width:100%">
-                <div><h1>Nome do Produto</h1></div>
-                <div><h4>R$500,00</h4></div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 
